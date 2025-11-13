3db73b38f616636c03a056cba54c19798d79404e
xcia4899 <xcia48996531@gmail.com>
2025-11-13 15:49:05 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <HomeView/>
+</template>
+
+<script setup lang="ts">
+import HomeView from './pages/Home.vue';
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Home View</h1>
+    {{userStore.token}}
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useUserStore } from '@/stores/user';
+const userStore = useUserStore();
+</script>
+
+<style scoped lang="scss">
+
+</style>
