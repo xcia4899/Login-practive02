4ac7a55ae3519be753828b2c4127d388a8cadd4f
xcia4899 <xcia48996531@gmail.com>
2025-11-14 10:59:43 +0800

    插件/設定安裝完成

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,18 @@
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './assets/styles/main.scss'  // 在這引入
 
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus'
+
+// import 'element-plus/dist/index.css'
+import './assets/styles/main.scss'  // 在這引入
+import "./assets/tailwind.css";
+import 'element-plus/theme-chalk/src/index.scss'
 
 const app = createApp(App)
+app.use(ElementPlus)
 
 app.use(createPinia())
 app.use(router)
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <h1>Home View</h1>
-    {{userStore.token}}
+    {{ userStore.token }}
+    <el-button type="primary">按鈕</el-button>
   </div>
+  <div class="p-2 bg-red-100 text-white text-2xl font-bold ">
+  Tailwind v4 OK
+</div>
 </template>
 
 <script setup lang="ts">
-import { useUserStore } from '@/stores/user';
+import { useUserStore } from "@/stores/user";
+// import { ElButton } from "element-plus";
 const userStore = useUserStore();
 </script>
 
-<style scoped lang="scss">
-
-</style>
+<style scoped lang="scss"></style>
